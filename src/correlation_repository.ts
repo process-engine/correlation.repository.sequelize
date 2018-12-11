90bccd78c3507c627cb50dcd50f7da0f51d83617
ElRaptorus <christian.werner@5minds.de>
2018-12-11 09:32:20 +0100

    :lipstick: Improve some logging

--- src/correlation_repository.ts
+++ src/correlation_repository.ts
@@ -34,7 +34,7 @@ export class CorrelationRepository implements ICorrelationRepository, IDisposabl
     logger.verbose('Initializing Sequelize connection and loading models...');
     const connectionAlreadyEstablished: boolean = this._sequelize !== undefined;
     if (connectionAlreadyEstablished) {
-      logger.verbose('Repository already initialized.');
+      logger.verbose('Repository already initialized. Done.');
 
       return;
     }
@@ -49,7 +49,7 @@ export class CorrelationRepository implements ICorrelationRepository, IDisposabl
     logger.verbose('Disposing connection');
     await this._connectionManager.destroyConnection(this.config);
     this._sequelize = undefined;
-    logger.verbose('done');
+    logger.verbose('Done.');
   }
 
   public async createEntry(identity: IIdentity,
